3609f99ff1601abd799b64a7887d64ad06117e35
MikeBLambert <mlamb128@gmail.com>
2019-03-02 11:46:02 -0800

    Create Qutoe and Exchange models and connect to mongodb

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-const getBuyPrices = () => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            const str = 'first';
-            resolve(str);
-        }, 3000);
-    });
-};
+require('dotenv').config();
+const connect = require('./lib/utils/connect');
+const Exchange = require('./lib/models/Exchange');
+connect();
 
-const getSellPrices = () => {
-    return new Promise(function(resolve, reject) {
-        setTimeout(() => {
-            const str2 = 'second';
-            resolve(str2);
-        }, 1000);
-    });
-};
 
-getBuyPrices()
-    .then(res => {
-        console.log(res);
-    })
-    .then(() => getSellPrices())
-    .then(res => console.log(res));
+const testDbCall = () => Exchange.create({
+    name: 'Coinbase',
+    fees: {
+        txCost: 15,
+        transferCost: 5
+    }
+});
+
+testDbCall();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getBuyPrices = () => {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             const str = 'first';
+//             resolve(str);
+//         }, 3000);
+//     });
+// };
+
+// const getSellPrices = () => {
+//     return new Promise(function(resolve, reject) {
+//         setTimeout(() => {
+//             const str2 = 'second';
+//             resolve(str2);
+//         }, 1000);
+//     });
+// };
+
+// getBuyPrices()
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .then(() => getSellPrices())
+//     .then(res => console.log(res));
