df8139d060060c478c9d6b080c11cefc43c2b35f
mlamb <mlamb128@gmail.com>
2019-04-10 20:31:31 -0700

    Added three exchanges and 6 coins with a util function to calculate profit percentage

--- index.js
+++ index.js
@@ -1,46 +1,57 @@
 require('dotenv').config();
-const connect = require('./lib/utils/connect');
-const Exchange = require('./lib/models/Exchange');
-const GDAX = require('gdax');
+const { getBinancePrice } = require('./lib/exchanges/binance');
+const { getCoinbasePrice } = require('./lib/exchanges/coinbase');
+const { getKrakenPrice } = require('./lib/exchanges/kraken');
+const { calculateProfitPercentage } = require('./lib/utils/utils');
 
-// connect();
+const comparePrices = async() => {
+    const currencyPair = [
+        { coinbase: 'ETH-BTC', binance: 'ETHBTC', kraken: 'XETHXXBT' },
+        { coinbase: 'LTC-BTC', binance: 'LTCBTC', kraken: 'XLTCXXBT' },
+        { coinbase: 'XRP-BTC', binance: 'XRPBTC', kraken: 'XXRPXXBT' },
+        { coinbase: 'XLM-BTC', binance: 'XLMBTC', kraken: 'XXLMXXBT' },
+        { coinbase: 'ETC-BTC', binance: 'ETCBTC', kraken: 'XETCXXBT' },
+        { binance: 'REPBTC', kraken: 'XREPXXBT' },
+    ];
 
-const authenticatedClient = new GDAX.AuthenticatedClient(
-  process.env.GDAX_API_KEY,
-  process.env.GDAX_SECRET,
-  process.env.GDAX_PASSPHRASE,
-  'https://api.pro.coinbase.com'
-);
+    for(let i = 0; i < currencyPair.length; i++) {
+        const binancePrice = await getBinancePrice(currencyPair[i].binance);
+        const coinbasePrice = await getCoinbasePrice(currencyPair[i].coinbase);
+        const krakenPrice = await getKrakenPrice(currencyPair[i].kraken);
 
-const uglyGetPrices = () => {
-  return new Promise((resolve, reject) => {
-    authenticatedClient.getProductTicker('ETH-USD', (error, response, data) => {
-      reject(error);
-      resolve(data);
-    });
-  });
+        const bcProfitPercent = calculateProfitPercentage(
+            coinbasePrice,
+            binancePrice,
+        );
+
+        const cbProfitPercent = calculateProfitPercentage(
+            binancePrice,
+            coinbasePrice,
+        );
+        const kbProfitPercent = calculateProfitPercentage(
+            binancePrice,
+            krakenPrice,
+        );
+        const bkProfitPercent = calculateProfitPercentage(
+            krakenPrice,
+            binancePrice,
+        );
+        const ckProfitPercent = calculateProfitPercentage(
+            krakenPrice,
+            coinbasePrice,
+        );
+        const kcProfitPercent = calculateProfitPercentage(
+            coinbasePrice,
+            krakenPrice,
+        );
+
+        console.log('bc', currencyPair[i].binance, bcProfitPercent);
+        console.log('cb', currencyPair[i].binance, cbProfitPercent);
+        console.log('kb', currencyPair[i].binance, kbProfitPercent);
+        console.log('bk', currencyPair[i].binance, bkProfitPercent);
+        console.log('ck', currencyPair[i].binance, ckProfitPercent);
+        console.log('kc', currencyPair[i].binance, kcProfitPercent);
+    }
 };
-console.log(uglyGetPrices());
 
-// const getPrices = () => {
-//   return new Promise((resolve, reject) => {
-//     authenticatedClient.getProductTicker(
-//       'ETH-USD',
-//       unwrapResponse(resolve, reject)
-//     );
-//   });
-// };
-// console.log('get prices:', getPrices);
-
-const comparePrices = async () => {
-  const cb = await uglyGetPrices();
-  console.log('our cb: ', cb);
-};
-// const unwrapResponse = (resolve, reject) => {
-//   return (err, response, data) => {
-//     err || !response
-//       ? reject(err || new Error('Nothing happened!'))
-//       : resolve(data);
-//   };
-// };
 comparePrices();
