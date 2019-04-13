70530a4cf25546e029d79eea84be53c1dec2185d
Luces Huayhuaca <luces.huayhuaca@gmail.com>
2019-04-13 13:03:20 -0700

    Progress towards formatting currency pairs to be able to pass into quote getting functions

--- index.js
+++ index.js
@@ -1,77 +1,76 @@
 require('dotenv').config();
-const { getBinancePrice } = require('./lib/exchanges/binance');
-const { getCoinbasePrice } = require('./lib/exchanges/coinbase');
-const { getKrakenPrice } = require('./lib/exchanges/kraken');
+const { getBinanceQuote } = require('./lib/exchanges/binance');
+const { getCoinbaseQuote } = require('./lib/exchanges/coinbase');
+const {
+    getKrakenQuote,
+    getAllCurrencyPairs
+} = require('./lib/exchanges/kraken');
 const { calculateProfitPercentage } = require('./lib/utils/utils');
 
-const comparePrices = async() => {
-    const currencyPair = [
-        { coinbase: 'ETH-BTC', binance: 'ETHBTC', kraken: 'XETHXXBT' },
-        { coinbase: 'LTC-BTC', binance: 'LTCBTC', kraken: 'XLTCXXBT' },
-        { coinbase: 'XRP-BTC', binance: 'XRPBTC', kraken: 'XXRPXXBT' },
-        { coinbase: 'XLM-BTC', binance: 'XLMBTC', kraken: 'XXLMXXBT' },
-        { coinbase: 'ETC-BTC', binance: 'ETCBTC', kraken: 'XETCXXBT' },
-        { binance: 'REPBTC', kraken: 'XREPXXBT' },
-    ];
+const compareQuotes = async() => {
+    const allCurrencyPairs = await getAllCurrencyPairs();
+    const formattedCurrencyPairs = allCurrencyPairs.map(currency => {
+        
+        return { coinbase: currency, binance: currency, kraken: currency };
+    });
+    console.log(formattedCurrencyPairs);
 
-    const allQuotes = {};
+    // for(let i = 0; i < allCurrencyPairs.length; i++) {
+    //     const binanceQuote = await getBinanceQuote(currencyPair[i].binance);
+    //     const coinbaseQuote = await getCoinbaseQuote(currencyPair[i].coinbase);
+    //     const krakenQuote = await getKrakenQuote(currencyPair[i].kraken);
 
-    for(let i = 0; i < currencyPair.length; i++) {
-        const binancePrice = await getBinancePrice(currencyPair[i].binance);
-        const coinbasePrice = await getCoinbasePrice(currencyPair[i].coinbase);
-        const krakenPrice = await getKrakenPrice(currencyPair[i].kraken);
+    // const bcProfitPercent = calculateProfitPercentage(
+    //     coinbaseQuote,
+    //     binanceQuote
+    // );
 
-        const bcProfitPercent = calculateProfitPercentage(
-            coinbasePrice,
-            binancePrice,
-        );
+    // const cbProfitPercent = calculateProfitPercentage(
+    //     binanceQuote,
+    //     coinbaseQuote
+    // );
+    // const kbProfitPercent = calculateProfitPercentage(
+    //     binanceQuote,
+    //     krakenQuote
+    // );
+    // const bkProfitPercent = calculateProfitPercentage(
+    //     krakenQuote,
+    //     binanceQuote
+    // );
+    // const ckProfitPercent = calculateProfitPercentage(
+    //     krakenQuote,
+    //     coinbaseQuote
+    // );
+    // const kcProfitPercent = calculateProfitPercentage(
+    //     coinbaseQuote,
+    //     krakenQuote
+    // );
 
-        const cbProfitPercent = calculateProfitPercentage(
-            binancePrice,
-            coinbasePrice,
-        );
-        const kbProfitPercent = calculateProfitPercentage(
-            binancePrice,
-            krakenPrice,
-        );
-        const bkProfitPercent = calculateProfitPercentage(
-            krakenPrice,
-            binancePrice,
-        );
-        const ckProfitPercent = calculateProfitPercentage(
-            krakenPrice,
-            coinbasePrice,
-        );
-        const kcProfitPercent = calculateProfitPercentage(
-            coinbasePrice,
-            krakenPrice,
-        );
-
-        {
-            bcProfitPercent &&
-                console.log('bc', currencyPair[i].binance, bcProfitPercent);
-        }
-        {
-            cbProfitPercent &&
-                console.log('cb', currencyPair[i].binance, cbProfitPercent);
-        }
-        {
-            kbProfitPercent &&
-                console.log('kb', currencyPair[i].binance, kbProfitPercent);
-        }
-        {
-            bkProfitPercent &&
-                console.log('bk', currencyPair[i].binance, bkProfitPercent);
-        }
-        {
-            ckProfitPercent &&
-                console.log('ck', currencyPair[i].binance, ckProfitPercent);
-        }
-        {
-            kcProfitPercent &&
-                console.log('kc', currencyPair[i].binance, kcProfitPercent);
-        }
-    }
+    // {
+    //     bcProfitPercent &&
+    //         console.log('bc', currencyPair[i].binance, bcProfitPercent);
+    // }
+    // {
+    //     cbProfitPercent &&
+    //         console.log('cb', currencyPair[i].binance, cbProfitPercent);
+    // }
+    // {
+    //     kbProfitPercent &&
+    //         console.log('kb', currencyPair[i].binance, kbProfitPercent);
+    // }
+    // {
+    //     bkProfitPercent &&
+    //         console.log('bk', currencyPair[i].binance, bkProfitPercent);
+    // }
+    // {
+    //     ckProfitPercent &&
+    //         console.log('ck', currencyPair[i].binance, ckProfitPercent);
+    // }
+    // {
+    //     kcProfitPercent &&
+    //         console.log('kc', currencyPair[i].binance, kcProfitPercent);
+    // }
+    // }
 };
 
-comparePrices();
+compareQuotes();
